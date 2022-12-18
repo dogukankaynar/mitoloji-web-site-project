735a3bffc27acbd3a7737bdd8681ee0871dc3bd8
dogukankaynar <dogukankaynar98@gmail.com>
2022-12-19 00:37:48 +0300

    showCard functionda template literals kullanılarak dinamik bir şekilde card oluştur

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-let cardTitle = document.getElementById('card-title');
-let cardDescription = document.getElementById('card-description');
-let cardImg = document.getElementById('card-img');
-console.log(cardDescription);
+let cardTitle = document.getElementsByClassName('card-title');
+let cardDescription = document.getElementsByClassName('card-description');
+let cardImg = document.getElementsByClassName('card-img');
+let box = document.getElementById('box')
 const getCard = new GetCard();
 console.log(getCard);
 function showCard(){
-cardTitle.textContent=getCard.card[0].title;
-cardDescription.textContent=getCard.card[0].description;
-cardImg.src=`${getCard.card[0].imgUrl}`
+    for(let i = 0;i< getCard.card.length;i++){
+        box.innerHTML += `
+        <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img id="card-img" class="card-img" src="${getCard.card[i].imgUrl}" alt="Avatar" />
+          </div>
+          <div class="flip-card-back">
+            <h1>${getCard.card[i].title}</h1>
+            <p>${getCard.card[i].description}</p>
+          </div>
+        </div>
+        </div>`
+    }
 }
 showCard();
