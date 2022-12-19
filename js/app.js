a64a9650cd9499195d677368fbf5be3d9d142033
dogukankaynar <dogukankaynar98@gmail.com>
2022-12-19 03:27:12 +0300

    remove ui.js

--- js/app.js
+++ js/app.js
@@ -3,20 +3,20 @@ let content = document.getElementById("content");
 const getCard = new GetCard();
 console.log(getCard);
 function showContent(){
-  content.innerHTML+=`
-  <h1 class="content-title">${getCard.contentText[0].title}</h1>
-  <p class="content-description">${getCard.contentText[0].description}</p>
-  `
-}
-function showCard() {
+    content.innerHTML+=`
+    <h1 class="content-title">${getCard.contentText[0].title}</h1>
+    <p class="content-description">${getCard.contentText[0].description}</p>
+    `
+  }
+  function showCard() {
   let i = 0;
   while (i < getCard.card.length) {
-    box.innerHTML += `
-        <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-            <img id="card-img" class="card-img" src="${getCard.card[i].imgUrl}" alt="Avatar" />
-          </div>
+      box.innerHTML += `
+          <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img id="card-img" class="card-img" src="${getCard.card[i].imgUrl}" alt="Avatar" />
+            </div>
           <div class="flip-card-back">
             <h1>${getCard.card[i].title}</h1>
             <p>${getCard.card[i].description}</p>
@@ -28,7 +28,3 @@ function showCard() {
 }
 showCard();
 showContent();
-function test() {
-  console.log("çalistim");
-}
-addEventListener("DOMContentLoaded",test)
